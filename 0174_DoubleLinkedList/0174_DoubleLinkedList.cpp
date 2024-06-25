db3e29b0bfe4288dd192b86f1e021d6c38e4aa9c
thekill022 <gibfathoni@gmail.com>
2024-06-25 21:30:09 +0700

    deklarasi pointer next dan pref untuk penunjukan data sebelum dan sesudah

--- 0174_DoubleLinkedList/0174_DoubleLinkedList.cpp
+++ 0174_DoubleLinkedList/0174_DoubleLinkedList.cpp
@@ -7,6 +7,9 @@ struct Node
     //deklarasi noMhs dan name untuk menampung data
     int noMhs;
     string name;
+    //deklarasi pointer next dan pref untuk penunjukan data sebelum dan sesudah
+    Node* next;
+    Node* prev;
 };
 
 int main()
