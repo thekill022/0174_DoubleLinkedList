b91e913937f154190255f57af1eeb935f97a4d4d
thekill022 <gibfathoni@gmail.com>
2024-06-25 21:46:04 +0700

    prosedur traverse untuk menampilkan data secara urut

--- 0174_DoubleLinkedList/0174_DoubleLinkedList.cpp
+++ 0174_DoubleLinkedList/0174_DoubleLinkedList.cpp
@@ -160,6 +160,23 @@ bool listEmpty()
     return (START == NULL);
 }
 
+//prosedur traverse untuk menampilkan data secara urut
+void traverse()
+{
+    if (listEmpty())
+        cout << "\nList is empty" << endl;
+    else
+    {
+        cout << "\nRecords in ascending order of roll number are:" << endl;
+        Node* currentNode = START; // step 1
+        while (currentNode != NULL) // step 2
+        {
+            cout << currentNode->noMhs << " " << currentNode->name << endl; // step 3
+            currentNode = currentNode->next;                                // step 4
+        }
+    }
+}
+
 int main()
 {
     
