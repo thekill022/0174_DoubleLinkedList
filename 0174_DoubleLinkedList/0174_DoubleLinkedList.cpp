54f7e6d068f0c5123a8ad9b1024974e4819f2bc6
thekill022 <gibfathoni@gmail.com>
2024-06-25 21:31:47 +0700

    pembuatan node dan pemberian value untuk data noMhs dan name

--- 0174_DoubleLinkedList/0174_DoubleLinkedList.cpp
+++ 0174_DoubleLinkedList/0174_DoubleLinkedList.cpp
@@ -19,6 +19,13 @@ Node* START = NULL;
 void addNode()
 {
 
+    //pembuatan node dan pemberian value untuk data noMhs dan name
+    Node* newNode = new Node(); // step 1 Buat Node baru
+    cout << "\nEnter the roll number of the student: ";
+    cin >> newNode->noMhs; //Assign value to the data field of the new node
+    cout << "\nEnter the name of the student: ";
+    cin >> newNode->name;  //Assign value to the data field of th
+
 }
 
 int main()
