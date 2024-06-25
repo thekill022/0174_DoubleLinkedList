586225fc7e51e8f504ff16c7a969dda848b6a2f5
thekill022 <gibfathoni@gmail.com>
2024-06-25 21:29:42 +0700

    deklarasi noMhs dan name untuk menampung data

--- 0174_DoubleLinkedList/0174_DoubleLinkedList.cpp
+++ 0174_DoubleLinkedList/0174_DoubleLinkedList.cpp
@@ -4,7 +4,9 @@ using namespace std;
 //membuat struktur untuk object double linkedList
 struct Node
 {
-    
+    //deklarasi noMhs dan name untuk menampung data
+    int noMhs;
+    string name;
 };
 
 int main()
