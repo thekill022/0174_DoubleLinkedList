5cf451250bd5541969b33b31b173bed38bcfcc6e
thekill022 <gibfathoni@gmail.com>
2024-06-25 21:43:55 +0700

    pembuatan function search untuk mencari data

--- 0174_DoubleLinkedList/0174_DoubleLinkedList.cpp
+++ 0174_DoubleLinkedList/0174_DoubleLinkedList.cpp
@@ -84,7 +84,19 @@ void addNode()
             START = newNode;
         }
     }
+}
 
+//pembuatan function search untuk mencari data
+bool search(int rollNo, Node** previous, Node** current)
+{
+    *previous = NULL;
+    *current = START;
+    while (*current != NULL && (*current)->noMhs != rollNo)
+    {
+        *previous = *current;
+        *current = (*current)->next;
+    }
+    return (*current != NULL);
 }
 
 int main()
