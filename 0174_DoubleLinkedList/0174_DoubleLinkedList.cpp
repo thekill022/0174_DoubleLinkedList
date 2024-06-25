0dc05581232491345b89c56953e1c9ee2b52bd0b
thekill022 <gibfathoni@gmail.com>
2024-06-25 21:38:52 +0700

    looping selama current != null dan noMhs dari current lebih kecil dari noMhs node baru

--- 0174_DoubleLinkedList/0174_DoubleLinkedList.cpp
+++ 0174_DoubleLinkedList/0174_DoubleLinkedList.cpp
@@ -55,6 +55,14 @@ void addNode()
         //set nilai current = start dan nilai previous = null
         Node* current = START; // step 1.a: start from the first node
         Node* previous = NULL; // step 1.b: previous node is Null initially
+
+        //looping selama current != null dan noMhs dari current lebih kecil dari noMhs node baru
+        while (current != NULL && current->noMhs < newNode->noMhs)
+        {                       // step 1.c: traverse the list to find the correct positionn
+            previous = current; // step 1.d: move the previous to the current node
+            current = current->next; // step 1.e: move the current to the next node
+        }
+
     }
 
 }
