79eb2d446f3e2b59a73ab3f56227cd5ef0b38435
thekill022 <gibfathoni@gmail.com>
2024-06-25 21:32:39 +0700

    kondisi jika  star tidak kosong dan noMhs node baru sama dengan noMhs start

--- 0174_DoubleLinkedList/0174_DoubleLinkedList.cpp
+++ 0174_DoubleLinkedList/0174_DoubleLinkedList.cpp
@@ -30,7 +30,13 @@ void addNode()
     //kondisi jika star == null atau noMhs node baru <= noMhs start
     if (START == NULL || newNode->noMhs <= START->noMhs)
     {
-
+        // step 2: insert the new node at the beginning
+        //kondisi jika  star tidak kosong dan noMhs node baru sama dengan noMhs start
+        if (START != NULL && newNode->noMhs == START->noMhs)
+        {
+            cout << "\033[31mDuplicate roll numbers not allowed\033[0m" << endl;
+            return;
+        }
     }
 
 }
