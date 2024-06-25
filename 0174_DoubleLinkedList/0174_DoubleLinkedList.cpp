ac2b4dc5895886e6e04a5fda7aab40e9eeb03d0d
thekill022 <gibfathoni@gmail.com>
2024-06-25 21:50:52 +0700

    prosedur untuk mencari data dan menampilkan data yang dicari jika ada

--- 0174_DoubleLinkedList/0174_DoubleLinkedList.cpp
+++ 0174_DoubleLinkedList/0174_DoubleLinkedList.cpp
@@ -197,6 +197,28 @@ void revtraverse()
     }
 }
 
+//prosedur untuk mencari data dan menampilkan data yang dicari jika ada
+void searchData()
+{
+    if (listEmpty() == true)
+    {
+        cout << "\nList is empty" << endl;
+    }
+    Node* prev, * curr;
+    prev = curr = NULL;
+    cout << "\nEnter the roll number of the student whose record you want to search: ";
+    int num;
+    cin >> num;
+    if (search(num, &prev, &curr) == false)
+        cout << "\nRecord not found" << endl;
+    else
+    {
+        cout << "\nRecord found" << endl;
+        cout << "\nRoll number: " << curr->noMhs << endl;
+        cout << "\nName: " << curr->name << endl;
+    }
+}
+
 int main()
 {
     
